daf39f01eee4a79050924f9bb41c2d9776162536
Caio Lopes de Carvalho <WillowBosom@users.noreply.github.com>
2016-08-26 10:21:44 -0300

    Add files via upload

---
+++ 3bim/atividade2/Conteudo/sobre.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>      </title>
+        meta charset="utf-8">
+        <link rel="stylesheet" href="sobre.css" type="text/css" />
+        <script type="text/javascript" src="sobre.js"></script>
+    </head>
+    <body>
+    <div class="conteudo">
+        <p id="p1"> Submarino é uma empresa brasileira. É uma das pioneiras no segmento de comércio eletrônico do Brasil, criada em 1999 e hoje, após fusão com a Americanas.com, é líder neste segmento. E é também uma loja virtual oficializada pela ABPD, juntamente IFPI </p>
+        <p id="p2"> Sua história começou quando Antonio Bonchristiano (presidente), Marcelo Ballona (vice-presidente de marketing) e Flavio Jansen (diretor de tecnologia) a idealizaram criando a empresa TBL S.A. Em vez de começar do zero, e como parte do investimento inicial, compraram uma das primeiras e maiores livrarias virtuais da época, a Booknet. Adquiriam, assim, os seus ativos, a marca, o site de domínio da empresa, uma carteira de 50 mil clientes cadastrados e a experiência de trabalho de mais de quatro anos no ramo.</p>
+    </div>
+    </body>
+</html>
